16d655fdb9db1dc7e063256fbf6ba74bd2ab5da3
Philipp Zach <philippzach@Philipps-Mini.Home>
2024-07-07 18:56:58 +0100

    added videos

--- components/archive-grid.js
+++ components/archive-grid.js
@@ -2,15 +2,19 @@ import { Image as DatocmsImage } from 'react-datocms';
 
 export default function ArchiveGrid({ archives }) {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+    <div className='columns-1 md:columns-2 lg:columns-3 gap-4'>
       {archives.map((archive) => (
-        <div key={archive.slug} className='relative bg-gray-200'>
+        <div
+          key={archive.slug}
+          className='break-inside-avoid mb-4'
+          style={{ breakInside: 'avoid' }}
+        >
           <DatocmsImage
             data={{
               ...archive.media.responsiveImage,
               alt: `Cover Image for ${archive.slug}`,
             }}
-            className='w-full h-full object-cover'
+            className='w-auto h-auto object-cover'
           />
         </div>
       ))}
